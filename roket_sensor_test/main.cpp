694f880b70eebd946889784863b605f4ce610fce
karaketir16 <osmankaraketir@gmail.com>
2023-02-27 12:35:51 +0300

    changes

--- roket_sensor_test/main.cpp
+++ roket_sensor_test/main.cpp
@@ -8,14 +8,19 @@
 
 #include "tusb.h"
 
-#include "MPU6050.h"
-#include <Adafruit_BMP3XX.h>
+#include <Adafruit_MPU6050.h>
+#include <Wire.h>
 
-#define UART_ID uart0
-#define BAUD_RATE 115200
 
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
+#include "Adafruit_BMP3XX.h"
+
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+
+
 
 static void core1_entry()
 {
@@ -31,35 +36,19 @@ static void core1_entry()
     }
 }
 
-int main_2();
-int main_scan();
+Adafruit_MPU6050 mpu;
+void mpu6050_setup();
+void mpu6050_loop();
 
-#define OUTPUT_READABLE_ACCELGYRO
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-// MPU6050 accelgyro;
-// MPU6050 accelgyro(0x69); // <-- use for AD0 high
-// MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
-
-static int16_t ax, ay, az;
-static int16_t gx, gy, gz;
-
-static MPU6050 mpu;
-
-#define GRAVITY 9.80665
+Adafruit_BMP3XX bmp;
+void bmp3xx_setup();
+void bmp3xx_loop();
 
 int main()
 {
     stdio_init_all();
     multicore_launch_core1(core1_entry);
-
-    uart_init(UART_ID, BAUD_RATE);
-
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
     uint32_t t0, t1;
 
@@ -70,51 +59,182 @@ int main()
     t1 = time_us_32();
     printf("\nusb host detected! (%dus)\n", t1 - t0);
 
-    // initialize device
-    printf("Initializing I2C devices...\r\n");
-    mpu.initialize();
+    mpu6050_setup();
+    bmp3xx_setup();
+    while(1){
+        t0 = time_us_32();
+        
+        {
+            mpu6050_loop();
+            bmp3xx_loop();
+        }
 
-    // verify connection
-    printf("Testing device connections...\r\n");
-    printf("%s\r\n", mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+        t1 = time_us_32();
+        
+        printf("\nReadTime (%dus)\n", t1 - t0);
 
-    mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
-
-    while (1)
-    {
-        sleep_ms(250);
-        // read raw accel/gyro measurements from device
-        // mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-        mpu.getAcceleration(&ax, &ay, &az);
-        // these methods (and a few others) are also available
-        // accelgyro.getAcceleration(&ax, &ay, &az);
-        // accelgyro.getRotation(&gx, &gy, &gz);
-
-#ifdef OUTPUT_READABLE_ACCELGYRO
-        // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
-        Serial.print(ax);
-        Serial.print("\t");
-        Serial.print(ay);
-        Serial.print("\t");
-        Serial.print(az);
-        // Serial.print("\t");
-        // Serial.print(gx);
-        // Serial.print("\t");
-        // Serial.print(gy);
-        // Serial.print("\t");
-        // Serial.print(gz);
-
-        Serial.print("\ta/g:\t");
-        Serial.print(ax / 2048.0 * GRAVITY);
-        Serial.print("\t");
-        Serial.print(ay/ 2048.0 * GRAVITY);
-        Serial.print("\t");
-        Serial.println(az/ 2048.0 * GRAVITY);
-
-
-#endif
+        delay(500);
     }
-
     return 0;
+}
+
+
+
+
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+
+
+
+
+void mpu6050_setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
+
+void mpu6050_loop() {
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+}
+
+
+
+
+
+void bmp3xx_setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+}
+
+void bmp3xx_loop() {
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
 }
